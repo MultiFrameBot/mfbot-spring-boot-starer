ea54d3f8a405aed2e8e2e34fa3fb1b8755b9c491
linrol <linrol@77hub.com>
2020-10-22 13:17:41 +0800

    init project maven and starer config

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mfbot-spring-boot-starer
 多框架QQ机器人的spring boot starter，开销即用。
+
+施工中
---
+++ src/main/java/com/alinkeji/mfbot/EnableMfbot.java
@@ -0,0 +1,17 @@
+package com.alinkeji.mfbot;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+// @Import({AutoConfiguration.class})
+public @interface EnableMfbot {
+
+}
